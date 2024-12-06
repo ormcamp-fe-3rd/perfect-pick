11eb5c450e5b53e5b71bf59cd0f522a65a0f1961
YellowFiber <dsa020175@gmail.com>
2024-12-06 15:16:31 +0900

    header-레이아웃 초안 완성

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,36 +2,63 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header>
+    <header className="inner flex h-[121px] items-center justify-between border-b">
       <Link to={'/'}>
-        <h1>PERFECT PICK</h1>
+        <h1 className="font-BagelFatOne text-[2.5rem] text-[#1F2C3E]">
+          PERFECT PICK
+        </h1>
       </Link>
 
       <nav>
-        <ul>
-          <li>
-            <a href="">TOP10</a>
+        <ul className="flex">
+          <li className="mr-[60px]">
+            <Link to={''} className="hover:text-red">
+              TOP10
+            </Link>
+          </li>
+          <li className="mr-[60px]">
+            <Link to={''} className="hover:text-red">
+              Mobile
+            </Link>
+          </li>
+          <li className="mr-[60px]">
+            <Link to={''} className="hover:text-red">
+              Tablet
+            </Link>
+          </li>
+          <li className="mr-[60px]">
+            <Link to={''} className="hover:text-red">
+              Notebook
+            </Link>
           </li>
           <li>
-            <a href="">Mobile</a>
-          </li>
-          <li>
-            <a href="">Tablet</a>
-          </li>
-          <li>
-            <a href="">Notebook</a>
-          </li>
-          <li>
-            <a href="">Wearable</a>
+            <Link to={''} className="hover:text-red">
+              Wearable
+            </Link>
           </li>
         </ul>
       </nav>
 
-      <ul>
+      <ul className="flex">
+        <li className="mr-[25px]">
+          <Link to={''}>
+            <img src="../images/header/ico-search.svg" alt="통합검색" />
+          </Link>
+        </li>
+        <li className="mr-[25px]">
+          <Link to={'/cart'}>
+            <img src="../images/header/ico-cart.svg" alt="장바구니" />
+          </Link>
+        </li>
+        <li className="mr-[25px]">
+          <Link to={'/mypage'}>
+            <img src="../images/header/ico-mypage.svg" alt="마이페이지" />
+          </Link>
+        </li>
         <li>
-          <a href="">
-            <img src="../images/header/ico-search.svg" alt="" />
-          </a>
+          <Link to={'/login'}>
+            <img src="../images/header/ico-login.svg" alt="로그인" />
+          </Link>
         </li>
       </ul>
     </header>

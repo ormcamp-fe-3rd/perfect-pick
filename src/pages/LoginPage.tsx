1d670464aab701bd461137d3131270a589a1255c
YellowFiber <dsa020175@gmail.com>
2024-12-06 18:48:47 +0900

    header-경로 재설정

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,9 +1,6 @@
-import Header from '@/components/layout/Header/Header';
-
 function LoginPage() {
   return (
     <>
-      <Header />
       <h1>로그인</h1>
     </>
   );
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,5 +1,7 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ReactNode } from 'react';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 
+import Header from '@/components/layout/Header/Header';
 import CartPage from '@/pages/CartPage';
 import HomePage from '@/pages/HomePage';
 import LoginPage from '@/pages/LoginPage';
@@ -12,22 +14,42 @@ import RecommendPage from '@/pages/RecommendPage';
 import RegisterPage from '@/pages/RegisterPage';
 import TotalSearchPage from '@/pages/TotalSearchPage';
 
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  const location = useLocation();
+
+  // Header가 안나오는 페이지 설정
+  const excludedPaths: string[] = ['/non-member', '/register', '/login'];
+
+  return (
+    <>
+      {!excludedPaths.includes(location.pathname) && <Header />}
+      {children}
+    </>
+  );
+};
+
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/non-member" element={<NonMemberPage />} />
-        <Route path="/payment" element={<PaymentPage />} />
-        <Route path="/product-detail" element={<ProductDetailPage />} />
-        <Route path="/product-search" element={<ProductSearchPage />} />
-        <Route path="/recommend" element={<RecommendPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/total-search" element={<TotalSearchPage />} />
-        <Route path="/cart" element={<CartPage />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/non-member" element={<NonMemberPage />} />
+          <Route path="/payment" element={<PaymentPage />} />
+          <Route path="/product-detail" element={<ProductDetailPage />} />
+          <Route path="/product-search" element={<ProductSearchPage />} />
+          <Route path="/recommend" element={<RecommendPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/total-search" element={<TotalSearchPage />} />
+          <Route path="/cart" element={<CartPage />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 };

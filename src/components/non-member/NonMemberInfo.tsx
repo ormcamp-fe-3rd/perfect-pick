c14becd6aacc7adb495446c0f19d3f8d67ac905a
YellowFiber <dsa020175@gmail.com>
2024-12-10 17:40:43 +0900

    nonMember-서브 타이틀 컴포넌트 제작

---
+++ src/components/non-member/NonMemberInfo.tsx
@@ -0,0 +1,27 @@
+export default function NonMemberInfo() {
+  return (
+    <>
+      <h3>회원정보</h3>
+      <ul>
+        <li>
+          <strong>주문번호</strong>
+          <span>abc1234455569</span>
+        </li>
+        <li>
+          <strong>이름</strong>
+          <span>abc2222</span>
+        </li>
+        <li>
+          <strong>연락처</strong>
+          <span>010-0000-0000</span>
+        </li>
+        <li>
+          <strong>주소</strong>
+          <span>
+            서울 강남구 강남대로 324 역삼디오슈페리움 2층 모두의연구소
+          </span>
+        </li>
+      </ul>
+    </>
+  );
+}

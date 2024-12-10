a0bd7c602d181b1638426ba1c43fe0a555531d06
oratio100 <oratio100@gmail.com>
2024-12-10 17:09:30 +0900

    cart-페이지 구조 추가

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,8 +1,28 @@
+import CartTable from '@/components/cart/CartTable';
+import CheckBox from '@/components/cart/CheckBox';
+
 function CartPage() {
   return (
-    <>
-      <h1>장바구니</h1>
-    </>
+    <div className="mx-auto flex w-full max-w-[1400px] flex-col">
+      <div className="mt-16 w-full text-center text-3xl font-extrabold">
+        장바구니
+      </div>
+      <div className="mb-6 mt-5 flex w-full gap-4">
+        <CheckBox
+          size="size-6"
+          checkedColor="bg-yellow"
+          labelStyle="text-2xl leading-0"
+        >
+          모두 선택
+        </CheckBox>
+        <button className="h-[35px] w-[126px] rounded-[50px] bg-[#D9D9D9] text-2xl">
+          선택 삭제
+        </button>
+      </div>
+      <CartTable />
+      <div>안내문구</div>
+      <div>버튼</div>
+    </div>
   );
 }
 

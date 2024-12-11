0e79a0774d94f4ecdb66b31d9c39364144674e33
oratio100 <oratio100@gmail.com>
2024-12-11 23:10:18 +0900

    payment-결제수단 컴포넌트 추가

---
+++ src/components/payment/PaymentOptions.tsx
@@ -0,0 +1,23 @@
+import PaymentPrivacyModal from './PaymentPrivacyModal';
+
+export default function PaymentOptions() {
+  return (
+    <>
+      <div className="flex flex-col gap-5 text-2xl">
+        <button className="h-20 rounded-[10px] border">신용/체크카드</button>
+        <div className="flex justify-between gap-3">
+          <button className="h-20 flex-1 rounded-[10px] border">
+            토스페이
+          </button>
+          <button className="h-20 flex-1 rounded-[10px] border">
+            네이버페이
+          </button>
+          <button className="h-20 flex-1 rounded-[10px] border">
+            무통장입금
+          </button>
+        </div>
+        <PaymentPrivacyModal />
+      </div>
+    </>
+  );
+}
--- src/pages/PaymentPage.tsx
+++ src/pages/PaymentPage.tsx
@@ -1,4 +1,5 @@
 import OrderDetails from '@/components/payment/OrderDetails';
+import PaymentOptions from '@/components/payment/PaymentOptions';
 import ShippingAddress from '@/components/payment/ShippingAddress';
 
 function PaymentPage() {
@@ -12,13 +13,13 @@ function PaymentPage() {
         <div className="mb-10 text-[36px] lg:text-center">주문 정보</div>
         <OrderDetails />
       </div>
-      <div className="flex justify-between">
-        <div className="">
-          <div className="text-[36px] lg:text-center">결제 수단</div>
-          <span>컴포넌트</span>
+      <div className="flex justify-between gap-20">
+        <div className="flex-grow">
+          <div className="mb-10 text-[36px] lg:text-center">결제 수단</div>
+          <PaymentOptions />
         </div>
-        <div>
-          <div className="text-[36px] lg:text-center">할인 적용</div>
+        <div className="flex-grow">
+          <div className="mb-10 text-[36px] lg:text-center">할인 적용</div>
           <span>컴포넌트</span>
         </div>
       </div>

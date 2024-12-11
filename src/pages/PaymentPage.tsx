8c2aff18607b1599a03907be474479676bc284d8
oratio100 <oratio100@gmail.com>
2024-12-11 11:33:36 +0900

    payment-페이지 구조 작성

--- src/pages/PaymentPage.tsx
+++ src/pages/PaymentPage.tsx
@@ -1,8 +1,27 @@
 function PaymentPage() {
   return (
-    <>
-      <h1>결제</h1>
-    </>
+    <div className="mx-24 mb-24 flex flex-col gap-24">
+      <div>
+        <div className="text-[36px] lg:text-center">구매자/배송지 정보</div>
+        <span>체크박스 컴포넌트</span>
+        <span>컴포넌트</span>
+      </div>
+      <div>
+        <div className="text-[36px] lg:text-center">주문 정보</div>
+        <span>컴포넌트</span>
+      </div>
+      <div className="flex justify-between">
+        <div className="">
+          <div className="text-[36px] lg:text-center">결제 수단</div>
+          <span>컴포넌트</span>
+        </div>
+        <div>
+          <div className="text-[36px] lg:text-center">할인 적용</div>
+          <span>컴포넌트</span>
+        </div>
+      </div>
+      <div>결제하기</div>
+    </div>
   );
 }
 

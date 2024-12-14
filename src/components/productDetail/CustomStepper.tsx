d811cd4152649af6e4be9312b65a5fc12f0677da
oratio100 <oratio100@gmail.com>
2024-12-14 22:42:03 +0900

    detail-stepper 컴포넌트 수정

--- src/components/productDetail/CustomStepper.tsx
+++ src/components/productDetail/CustomStepper.tsx
@@ -1,17 +1,19 @@
 import { useState } from 'react';
 
 interface CustomStepperProps {
-  defaultValue?: number;
-  size?: string;
-  fontSize?: string;
   shape?: 'round' | 'square';
+  defaultValue?: number;
+  style?: string;
+  numberStyle?: string;
+  buttonStyle?: string;
 }
 
 export default function CustomStepper({
-  defaultValue = 1,
-  size = 'size-10',
-  fontSize,
   shape = 'square',
+  defaultValue = 1,
+  style,
+  numberStyle,
+  buttonStyle,
 }: CustomStepperProps) {
   const [amount, setAmount] = useState(defaultValue);
 
@@ -27,21 +29,21 @@ export default function CustomStepper({
 
   return (
     <div
-      className={`mx-3 flex w-full items-center justify-between gap-5 text-center text-lg font-semibold`}
+      className={`${style} flex w-full items-center justify-between gap-5 text-center text-lg font-semibold`}
     >
       <button
-        className={`${fontSize} flex ${size} items-center justify-center ${shapeProp} bg-[#D9D9D9]`}
+        className={`${buttonStyle} flex size-10 items-center justify-center ${shapeProp} bg-[#D9D9D9]`}
         onClick={() => adjustButtonClick(-1)}
       >
         -
       </button>
       <div
-        className={`${fontSize} flex h-10 items-center justify-center border-gray`}
+        className={`${numberStyle} flex h-10 items-center justify-center border-gray`}
       >
         {amount}
       </div>
       <button
-        className={`${fontSize} flex ${size} items-center justify-center ${shapeProp} bg-[#D9D9D9]`}
+        className={`${buttonStyle} flex size-10 items-center justify-center ${shapeProp} bg-[#D9D9D9]`}
         onClick={() => adjustButtonClick(1)}
       >
         +

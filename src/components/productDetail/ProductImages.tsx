0f5f8b420f9c327a5ce477b57ac5002064a86c11
oratio100 <oratio100@gmail.com>
2024-12-14 22:23:32 +0900

    detail-임시 변수 주석 처리

--- src/components/productDetail/ProductImages.tsx
+++ src/components/productDetail/ProductImages.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 
-export default function ProductImages() {
-  const imagesList = [
-    { id: '1', src: 'https://i.imgur.com/Jvh1OQmb.jpg', alt: 'image1' },
-    { id: '2', src: 'https://i.imgur.com/zADtGy9b.jpg', alt: 'image2' },
-    { id: '3', src: 'https://i.imgur.com/Ae1wefjb.jpg', alt: 'image3' },
-    { id: '4', src: 'https://i.imgur.com/svOz7A9b.jpg', alt: 'image4' },
-  ];
+// 임시: 추후 서버 연결
+const imagesList = [
+  { id: '1', src: 'https://i.imgur.com/Jvh1OQmb.jpg', alt: 'image1' },
+  { id: '2', src: 'https://i.imgur.com/zADtGy9b.jpg', alt: 'image2' },
+  { id: '3', src: 'https://i.imgur.com/Ae1wefjb.jpg', alt: 'image3' },
+  { id: '4', src: 'https://i.imgur.com/svOz7A9b.jpg', alt: 'image4' },
+];
 
+export default function ProductImages() {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const handleButtonClick = (index: number) => {

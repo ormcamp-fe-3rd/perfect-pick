f1e66c5e2cb6c4d514b327a861a99c28267da979
oratio100 <oratio100@gmail.com>
2024-12-15 22:43:25 +0900

    cart-피드백 반영: CartListItem 컴포넌트 분리

--- src/components/cart/CartTable.tsx
+++ src/components/cart/CartTable.tsx
@@ -1,42 +1,43 @@
 import { useState } from 'react';
 import CartCheckBox from '@/components/cart/CartCheckBox';
-import CustomStepper from '@/components/cart/CustomStepper';
+import CartListItem from './CartListItem';
+
+// 임시: 추후 서버 연결
+const cartList = [
+  {
+    id: 1,
+    name: '갤럭시 S24 FE 자급제',
+    src: 'https://i.imgur.com/svOz7A9b.jpg',
+    options: {
+      color: 'white',
+      storage: '256G',
+      accessories: 'PD 충전기 절전형(케이블 미포함)',
+    },
+    amount: 2,
+    price: {
+      productPrice: 896000,
+      accessoriesPrice: 15000,
+    },
+  },
+  {
+    id: 2,
+    name: '아이패드 프로 11인치 (Wi-Fi)',
+    src: 'https://i.imgur.com/Ae1wefjb.jpg',
+    options: { color: '스페이스 블랙', storage: '1T' },
+    amount: 1,
+    price: { productPrice: 2594000 },
+  },
+  {
+    id: 3,
+    name: '갤럭시 S24 FE 자급제',
+    src: 'https://i.imgur.com/svOz7A9b.jpg',
+    options: { color: 'white', storage: '512G' },
+    amount: 1,
+    price: { productPrice: 896000, deliveryFee: 3000 },
+  },
+];
 
 export default function CartTable() {
-  const cartList = [
-    {
-      id: 1,
-      name: '갤럭시 S24 FE 자급제',
-      src: 'https://i.imgur.com/svOz7A9b.jpg',
-      options: {
-        color: 'white',
-        storage: '256G',
-        accessories: 'PD 충전기 절전형(케이블 미포함)',
-      },
-      amount: 2,
-      price: {
-        productPrice: 896000,
-        accessoriesPrice: 15000,
-      },
-    },
-    {
-      id: 2,
-      name: '아이패드 프로 11인치 (Wi-Fi)',
-      src: 'https://i.imgur.com/Ae1wefjb.jpg',
-      options: { color: '스페이스 블랙', storage: '1T' },
-      amount: 1,
-      price: { productPrice: 2594000 },
-    },
-    {
-      id: 3,
-      name: '갤럭시 S24 FE 자급제',
-      src: 'https://i.imgur.com/svOz7A9b.jpg',
-      options: { color: 'white', storage: '512G' },
-      amount: 1,
-      price: { productPrice: 896000, deliveryFee: 3000 },
-    },
-  ];
-
   const allItems = new Set(cartList.map((item) => item.id));
   const [checkedItems, setCheckedItems] = useState(allItems);
 
@@ -101,65 +102,14 @@ export default function CartTable() {
         </div>
       </div>
       {cartList.map((item) => (
-        <div className="grid grid-cols-7 items-center border-b lg:grid-cols-1">
-          <div className="col-span-4 lg:col-span-1">
-            <div className="flex items-center gap-6 border-r p-6 md:px-2 lg:border-0 lg:px-10">
-              <CartCheckBox
-                checkedFormula={checkedItems.has(item.id)}
-                onChange={(isChecked) =>
-                  handleCheckboxChange(item.id, isChecked)
-                }
-              />
-              <div className="flex items-center gap-11 lg:gap-6">
-                <img
-                  className="size-[110px] rounded-[10px]"
-                  src={item.src}
-                  alt={`${item.name}'s image`}
-                />
-                <div className="flex flex-col justify-center gap-6">
-                  <div className="text-2xl font-semibold">{item.name}</div>
-                  <div className="text-lg font-semibold">
-                    옵션: {Object.values(item.options).join('/')}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="relative flex h-full items-center justify-center border-r lg:border-0">
-            <div className="absolute left-20 hidden text-2xl lg:block">
-              수량:
-            </div>
-            <div className="flex w-full lg:w-52">
-              <CustomStepper
-                shape="round"
-                size="size-10 lg:size-7"
-                fontSize="text-2xl lg:text-xl"
-                defaultValue={item.amount}
-              />
-            </div>
-          </div>
-          <div className="relative flex h-full items-center justify-center border-r py-3 text-2xl lg:border-0 lg:text-xl">
-            <div className="absolute left-20 hidden text-2xl lg:block">
-              상품금액:
-            </div>
-            {(
-              (item.price?.productPrice ?? 0) +
-              (item.price?.accessoriesPrice ?? 0)
-            ).toLocaleString()}
-            원
-          </div>
-          <div className="relative flex h-full items-center justify-center py-3 text-2xl lg:text-xl">
-            <div className="absolute left-20 hidden text-2xl lg:block">
-              배송비:
-            </div>
-            <div className="text-center text-2xl lg:text-xl">
-              {item.price?.deliveryFee ?? 0}원
-            </div>
-          </div>
-        </div>
+        <CartListItem
+          item={item}
+          checkedItems={checkedItems}
+          onCheckboxChange={handleCheckboxChange}
+        />
       ))}
-      <div className="flex w-full items-center justify-end gap-12 border-b py-2 pr-12 text-2xl font-semibold md:text-xl lg:flex-col lg:items-center lg:gap-3 lg:pr-6">
-        <div className="flex gap-12 md:flex-col lg:gap-3">
+      <div className="flex w-full items-center justify-end gap-12 border-b py-2 pr-12 text-2xl font-semibold lg:flex-col lg:items-center lg:gap-3 lg:pr-6 md:text-xl">
+        <div className="flex gap-12 lg:gap-3 md:flex-col">
           <span>상품금액 {totalPrice.toLocaleString()}원</span>
           <div className="flex justify-center gap-12 lg:gap-3">
             <span>+</span>

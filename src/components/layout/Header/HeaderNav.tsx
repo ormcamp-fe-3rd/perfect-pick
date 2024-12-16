2235b9dad2047f16ba459755ce162c08bfca9bf4
YellowFiber <dsa020175@gmail.com>
2024-12-16 11:51:59 +0900

    header 파일 추가

--- src/components/layout/Header/HeaderNav.tsx
+++ src/components/layout/Header/HeaderNav.tsx
@@ -4,46 +4,47 @@ interface NavProps {
   isMenuOpen: boolean;
 }
 
-export default function HeaderNav({ isMenuOpen }: NavProps) {
+export default function HeaderNav({isMenuOpen} : NavProps) {
+
   return (
     <nav className={`${isMenuOpen ? 'lg:hidden' : ''}`}>
-      <ul className="flex gap-[60px] lg:absolute lg:top-[176px] lg:w-[calc(100%-30px)] lg:gap-7 md:flex-wrap md:gap-[10px]">
-        <li className="lg:h-[50px] lg:w-full lg:rounded-full lg:bg-green">
+      <ul className="flex gap-[60px] md:flex-wrap md:gap-[10px] lg:absolute lg:top-[176px] lg:w-[calc(100%-30px)] lg:gap-7">
+        <li className="lg:bg-green lg:h-[50px] lg:w-full lg:rounded-full">
           <Link
             to={''}
-            className="block hover:text-red lg:flex lg:h-full lg:w-full lg:items-center lg:justify-center lg:text-center lg:text-2xl lg:font-semibold lg:hover:text-black"
+            className="hover:text-red block lg:flex lg:h-full lg:w-full lg:items-center lg:justify-center lg:text-center lg:text-2xl lg:font-semibold lg:hover:text-black"
           >
             TOP10
           </Link>
         </li>
-        <li className="lg:h-[50px] lg:w-full lg:rounded-full lg:bg-salmon">
+        <li className="lg:bg-salmon lg:h-[50px] lg:w-full lg:rounded-full">
           <Link
             to={''}
-            className="block hover:text-red lg:flex lg:h-full lg:w-full lg:items-center lg:justify-center lg:text-center lg:text-2xl lg:font-semibold lg:hover:text-black"
+            className="hover:text-red block lg:flex lg:h-full lg:w-full lg:items-center lg:justify-center lg:text-center lg:text-2xl lg:font-semibold lg:hover:text-black"
           >
             Mobile
           </Link>
         </li>
-        <li className="lg:h-[50px] lg:w-full lg:rounded-full lg:bg-yellow">
+        <li className="lg:bg-yellow lg:h-[50px] lg:w-full lg:rounded-full">
           <Link
             to={''}
-            className="block hover:text-red lg:flex lg:h-full lg:w-full lg:items-center lg:justify-center lg:text-center lg:text-2xl lg:font-semibold lg:hover:text-black"
+            className="hover:text-red block lg:flex lg:h-full lg:w-full lg:items-center lg:justify-center lg:text-center lg:text-2xl lg:font-semibold lg:hover:text-black"
           >
             Tablet
           </Link>
         </li>
-        <li className="lg:h-[50px] lg:w-full lg:rounded-full lg:bg-pink">
+        <li className="lg:bg-pink lg:h-[50px] lg:w-full lg:rounded-full">
           <Link
             to={''}
-            className="block hover:text-red lg:flex lg:h-full lg:w-full lg:items-center lg:justify-center lg:text-center lg:text-2xl lg:font-semibold lg:hover:text-black"
+            className="hover:text-red block lg:flex lg:h-full lg:w-full lg:items-center lg:justify-center lg:text-center lg:text-2xl lg:font-semibold lg:hover:text-black"
           >
             Notebook
           </Link>
         </li>
-        <li className="lg:h-[50px] lg:w-full lg:rounded-full lg:bg-skyblue">
+        <li className="lg:bg-skyblue lg:h-[50px] lg:w-full lg:rounded-full">
           <Link
             to={''}
-            className="block hover:text-red lg:flex lg:h-full lg:w-full lg:items-center lg:justify-center lg:text-center lg:text-2xl lg:font-semibold lg:hover:text-black"
+            className="hover:text-red block lg:flex lg:h-full lg:w-full lg:items-center lg:justify-center lg:text-center lg:text-2xl lg:font-semibold lg:hover:text-black"
           >
             Wearable
           </Link>

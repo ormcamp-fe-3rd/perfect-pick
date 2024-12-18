d097fc465f90c514ff04fd29f50d58c463d2e2f7
oratio100 <oratio100@gmail.com>
2024-12-18 21:14:31 +0900

    파일정리: firebase 설정, 불필요한 변수 정리

--- src/components/productDetail/ProductOptions.tsx
+++ src/components/productDetail/ProductOptions.tsx
@@ -1,5 +1,4 @@
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
 import ActionButton from '@/components/productDetail/ActionButton';
 import CustomStepper from '@/components/productDetail/CustomStepper';
 import SelectOption from '@/components/productDetail/SelectOption';

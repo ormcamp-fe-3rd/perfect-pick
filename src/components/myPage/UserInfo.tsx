8d862574f4e60d4bc753de1f02c4e42cc3d4fc29
oratio100 <oratio100@gmail.com>
2024-12-18 10:51:34 +0900

    mypage-form 태그로 변경

--- src/components/myPage/UserInfo.tsx
+++ src/components/myPage/UserInfo.tsx
@@ -16,47 +16,49 @@ export default function UserInfo() {
         <div className="w-1/3">이메일</div>
         <div className="w-2/3">{`이메일`}</div>
       </div>
-      <div>
-        <div>비밀번호</div>
-        <div className="mt-5 flex flex-col gap-[5px]">
-          <UserInfoInput
-            id="inputCurrentPw"
-            placeholder="현재 비밀번호를 입력해주세요."
-          />
-          <UserInfoInput
-            id="inputNewPw"
-            placeholder="새 비밀번호를 입력해주세요."
-          />
-          <UserInfoInput
-            id="inputNewPwAgain"
-            placeholder="새 비밀번호를 다시 입력해주세요."
-          />
+      <form action="" className="flex flex-col gap-[30px]">
+        <div>
+          <div>비밀번호</div>
+          <div className="mt-5 flex flex-col gap-[5px]">
+            <UserInfoInput
+              id="inputCurrentPw"
+              placeholder="현재 비밀번호를 입력해주세요."
+            />
+            <UserInfoInput
+              id="inputNewPw"
+              placeholder="새 비밀번호를 입력해주세요."
+            />
+            <UserInfoInput
+              id="inputNewPwAgain"
+              placeholder="새 비밀번호를 다시 입력해주세요."
+            />
+          </div>
         </div>
-      </div>
-      <div>
-        <div>주소</div>
-        <div className="mt-5">
-          기존 주소 :{' '}
-          {`서울 강남구 강남대로 324 역삼디오슈페리움 2층 모두의연구소`}
+        <div>
+          <div>주소</div>
+          <div className="mt-5">
+            기존 주소 :{' '}
+            {`서울 강남구 강남대로 324 역삼디오슈페리움 2층 모두의연구소`}
+          </div>
+          <div className="mt-[10px] flex flex-col gap-[5px]">
+            <UserInfoInput
+              id="inputBaseAddress"
+              placeholder="주소 찾기로 입력해주세요."
+              showButton={true}
+              buttonText="주소 찾기"
+              buttonId="findAddressButton"
+              onButtonClick={SearchAddress}
+            />
+            <UserInfoInput
+              id="inputDetailAddress"
+              placeholder="상세 주소를 입력해주세요."
+            />
+          </div>
         </div>
-        <div className="mt-[10px] flex flex-col gap-[5px]">
-          <UserInfoInput
-            id="inputBaseAddress"
-            placeholder="주소 찾기로 입력해주세요."
-            showButton={true}
-            buttonText="주소 찾기"
-            buttonId="findAddressButton"
-            onButtonClick={SearchAddress}
-          />
-          <UserInfoInput
-            id="inputDetailAddress"
-            placeholder="상세 주소를 입력해주세요."
-          />
-        </div>
-      </div>
-      <button className="h-[60px] w-full rounded-[50px] bg-black text-center text-lg text-white">
-        수정하기
-      </button>
+        <button className="h-[60px] w-full rounded-[50px] bg-black text-center text-lg text-white">
+          수정하기
+        </button>
+      </form>
     </div>
   );
 }

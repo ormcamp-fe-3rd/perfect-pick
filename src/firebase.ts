22a105cf99fda83f0c8cca2a80402cb91318f48f
oratio100 <oratio100@gmail.com>
2024-12-19 17:25:37 +0900

    파일정리: firebase 설정

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,12 +1,6 @@
-// Import the functions you need from the SDKs you need
-// import { getAnalytics } from 'firebase/analytics';
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
@@ -18,8 +12,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-const db = getFirestore(app);
-
-export { db };
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);

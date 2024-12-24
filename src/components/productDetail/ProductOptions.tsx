b39fd1487979b4e57dc3d07f79f6af63088eb6a5
oratio100 <oratio100@gmail.com>
2024-12-24 20:34:10 +0900

    피드백: sort 정렬 falsy 값 대응

--- src/components/productDetail/ProductOptions.tsx
+++ src/components/productDetail/ProductOptions.tsx
@@ -83,7 +83,13 @@ export default function ProductOptions({
   };
 
   const selectedOptionsLabel = Object.entries(selectedOptions)
-    .sort(([keyA], [keyB]) => keyA.localeCompare(keyB))
+    .sort(([keyA], [keyB]) => {
+      if (!!keyA && !!keyB) {
+        // keyA와 keyB가 모두 truthy인지 확인
+        return keyA.localeCompare(keyB);
+      }
+      return 0; // keyA 또는 keyB가 falsy라면 기본 정렬 순서를 유지
+    })
     .filter(([, value]) => value)
     .map(([, value]) => value)
     .join('/');

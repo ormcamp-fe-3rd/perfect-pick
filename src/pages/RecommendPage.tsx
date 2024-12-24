68111c21a9b027ee988ff11ac7dbdc1e664ce3ae
oratio100 <oratio100@gmail.com>
2024-12-24 22:41:22 +0900

    TOP10 페이지 안내문구 처리(서비스 준비중)

--- src/pages/RecommendPage.tsx
+++ src/pages/RecommendPage.tsx
@@ -1,7 +1,13 @@
 function RecommendPage() {
   return (
     <>
-      <h1>제품추천</h1>
+      <div className="mt-44 flex h-screen flex-col items-center gap-4 text-2xl">
+        <div>서비스가 준비중에 있습니다.</div>
+        <div>
+          더 나은 서비스를 제공하기 위해 최선을 다하고 있습니다. 이용에 불편을
+          드려 죄송하며, 빠른 시일 내에 찾아뵙겠습니다. 감사합니다.
+        </div>
+      </div>
     </>
   );
 }

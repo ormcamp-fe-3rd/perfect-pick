97bdb3ffbc2561e11e27e0cff3b522e90ee7b7c1
oratio100 <oratio100@gmail.com>
2024-12-26 14:44:28 +0900

    버그: 데이터 타입 수정

--- src/components/myPage/UserInfo.tsx
+++ src/components/myPage/UserInfo.tsx
@@ -1,16 +1,9 @@
 import { useEffect, useState } from 'react';
 
 import SearchAddress from '@/components/common/SearchAddress';
+import UserInfoInput from '@/components/myPage/UserInfoInput';
 import { getUserInfo, updateUserAddress, updateUserPassword } from '@/firebase';
-import UserInfoInput from './UserInfoInput';
-
-export interface User {
-  userid: string | null;
-  username: string | null;
-  email: string | null;
-  address: string | null;
-  details: string | null;
-}
+import { UserData } from '@/types';
 
 export default function UserInfo() {
   const [user, setUser] = useState<UserData | null>(null);
@@ -138,7 +131,7 @@ export default function UserInfo() {
         <div>
           <div>주소</div>
           <div className="mt-5">
-            기존 주소 : {user?.address} {user?.details}
+            기존 주소 : {user?.address} {user?.detailAddress}
           </div>
           <div className="mt-[10px] flex flex-col gap-[5px]">
             <UserInfoInput
